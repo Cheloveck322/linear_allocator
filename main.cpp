d7f278a7bf9bd6dfb1b1a584f4a68efa9bbf3181
Cheloveck322 <vasyagri2005@gmail.com>
2026-01-20 18:58:02 +0300

    adding matrix test

--- main.cpp
+++ main.cpp
@@ -21,5 +21,33 @@ int main()
 
     std::cout << durationMy << ' ' << durationSys << std::endl;
 
+    auto matrix = new int[10000][10000];
+    int sum = 0;
+
+    auto beginMatrix1 = std::chrono::steady_clock::now();
+    for (int i = 0; i < 10000; ++i)
+    {
+        for (int j = 0; j < 10000; ++j)
+        {
+            sum += matrix[i][j];
+        }
+    }
+    auto endMatrix1 = std::chrono::steady_clock::now();
+
+    auto beginMatrix2 = std::chrono::steady_clock::now();
+    for (int i = 0; i < 10000; ++i)
+    {
+        for (int j = 0; j < 10000; ++j)
+        {
+            sum += matrix[j][i];
+        }
+    }
+    auto endMatrix2 = std::chrono::steady_clock::now();
+
+    auto durationMatrix1 = std::chrono::duration_cast<std::chrono::nanoseconds>(endMatrix1 - beginMatrix1).count();
+    auto durationMatrix2 = std::chrono::duration_cast<std::chrono::nanoseconds>(endMatrix2 - beginMatrix2).count();
+
+    std::cout << durationMatrix1 << ' ' << durationMatrix2 << std::endl;
+    
     return 0;
 }
